ab6958f4b7e169fe439e49fcfd79b8afa2caf747
ralfoliveira@gmail.com <ralfoliveira@gmail.com>
2022-05-28 14:34:18 -0300

    fix: add cors policy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ origin: '*' });
   await app.listen(3333);
 }
 bootstrap();
