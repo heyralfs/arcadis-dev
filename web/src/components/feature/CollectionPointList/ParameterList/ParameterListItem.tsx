fa3be8a98467b1dd488e6a792adab1d4194c0dd4
ralfoliveira@gmail.com <ralfoliveira@gmail.com>
2022-05-29 16:29:36 -0300

    feat: delete parameter

--- web/src/components/feature/CollectionPointList/ParameterList/ParameterListItem.tsx
+++ web/src/components/feature/CollectionPointList/ParameterList/ParameterListItem.tsx
@@ -1,4 +1,16 @@
-import { Box, Flex, Text, Tooltip } from "@chakra-ui/react";
+import {
+	Box,
+	Flex,
+	IconButton,
+	Spinner,
+	Text,
+	Tooltip,
+	useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { BiTrash } from "react-icons/bi";
+import { useCollectionPointsContext } from "../../../../contexts/CollectionPointsContext/useCollectionPointsContext";
+import { api } from "../../../../services/axios";
 import { dateFormatter } from "../../../../services/dateFormatter";
 import { IParameter } from "../../../../types/CollectionPoint.interface";
 
@@ -7,7 +19,36 @@ interface ParameterListItemProps {
 }
 
 export function ParameterListItem({ parameter }: ParameterListItemProps) {
-	const { name, collectionDate, value, unit, overLimit } = parameter;
+	const { id, name, collectionDate, value, unit, overLimit } = parameter;
+	const { getCollectionPoints } = useCollectionPointsContext();
+	const toast = useToast();
+
+	const [loading, setLoading] = useState(false);
+
+	function handleDelete() {
+		setLoading(true);
+		api.delete(`parameters/${id}`)
+			.then(() => {
+				toast({
+					description: "Parâmetro excluído com sucesso!",
+					status: "success",
+					duration: 5000,
+					isClosable: true,
+				});
+				getCollectionPoints();
+			})
+			.catch((err) => {
+				toast({
+					description: err.response.data.message[0],
+					status: "error",
+					duration: 5000,
+					isClosable: true,
+				});
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	}
 
 	return (
 		<Tooltip label={overLimit ? "Parâmetro acima do limite permitido" : ""}>
@@ -27,6 +68,17 @@ export function ParameterListItem({ parameter }: ParameterListItemProps) {
 				</Box>
 				<Box flex="1" borderBottom="1px dotted" />
 				<Text>{`${value} ${unit}`}</Text>
+				<Tooltip label="Excluir">
+					<IconButton
+						variant="outline"
+						size="xs"
+						ml={2}
+						color="brand.400"
+						aria-label="Excluir"
+						icon={loading ? <Spinner size="xs" /> : <BiTrash />}
+						onClick={handleDelete}
+					/>
+				</Tooltip>
 			</Flex>
 		</Tooltip>
 	);
